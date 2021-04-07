f6840bcb998d61f7582bae96fe4b9d833a292850
piermorosini <info@immagineweb.net>
2021-04-07 22:36:41 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ app.route('/api/courses/:id').put(saveCourse);
 
 app.route('/api/login').post(loginUser);
 
-const httpServer = app.listen(() => {
+const httpServer = app.listen(8000, () => {
     console.log("HTTP REST API Server running at https://angular-university-courses.herokuapp.com/api/courses:" + httpServer.address()["port"]);
 });
 
