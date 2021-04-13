ece565307abd5af431b4be20f63d19f4d116dea3
piermorosini <info@immagineweb.net>
2021-04-13 16:57:56 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,9 +6,19 @@ import {searchLessons} from "./search-lessons.route";
 import {saveCourse} from './save-course.route';
 import {loginUser} from './login.route';
 
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 
 const app: Application = express();
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 
